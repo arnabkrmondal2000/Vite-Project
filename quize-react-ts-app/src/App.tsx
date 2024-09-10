2e75d6e015dedee815705ceed71b7c47bb47de67
Anuj_dev <anukumar@zuelligpharma.com>
2024-09-10 18:16:35 +0530

    '4th commit'

---
+++ quize-react-ts-app/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+//import React, { useState } from 'react'
+import QuizeDisplay from './component/QuizeDisplay/Quize';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <QuizeDisplay />
+    </>
+  )
+}
+
+export default App
