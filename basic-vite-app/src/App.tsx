055ffe0957b02779f22c27b61ab68edec819687b
Anuj_dev <anukumar@zuelligpharma.com>
2024-09-10 16:23:54 +0530

    first commit

---
+++ basic-vite-app/src/App.tsx
@@ -0,0 +1,15 @@
+//import React, { useState } from 'react'
+import QuizeDisplay from './component/QuizDisplay/Quize'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <QuizeDisplay />
+    </>
+  )
+}
+
+export default App
